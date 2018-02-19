4b904a0deeead6c5ec036f4e3dbd36b6016a6d75
Sudeep R <sudeep.r@urbanladder.com>
2018-02-19 10:24:07 +0530

    Initial Commit

---
+++ FoodVariant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodVariant
+//
+//  Created by sudeep.r on 19/02/18.
+//  Copyright © 2018 sudeep.r. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
